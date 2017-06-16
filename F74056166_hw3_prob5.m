b1f0be085e07448dd1b998109144fead9831c494
frank870622 <frank870622@gmail.com>
2017-06-16 12:07:47 +0800

    Add files via upload

--- F74056166_hw3_prob5.m
+++ F74056166_hw3_prob5.m
@@ -18,10 +18,10 @@ hold on
 for i = 1:10000
     ax = -G*M*x0/((x0^2)+(y0^2))^(3/2);                                         %a = F/m
     ay = -G*M*y0/((x0^2)+(y0^2))^(3/2);                                         %F = GMm/r^2
-    vx = vx0 + ax0*(T/365);                                                     %v = v0 + at
-    vy = vy0 + ay0*(T/365);                                                     %x = x0 + vt + 1/2*a*t^2
-    x = x0 + vx0*(T/365) + 1/2*ax0*((T/365)^2);
-    y = y0 + vy0*(T/365) + 1/2*ay0*((T/365)^2);
+    vx = vx0 + ax0*(T/10000);                                                     %v = v0 + at
+    vy = vy0 + ay0*(T/10000);                                                     %x = x0 + vt + 1/2*a*t^2
+    x = x0 + vx0*(T/10000) + 1/2*ax0*((T/10000)^2);
+    y = y0 + vy0*(T/10000) + 1/2*ay0*((T/10000)^2);
     ax0 = ax;
     ay0 = ay;
     vx0 = vx;
@@ -47,10 +47,10 @@ hold on
 for i = 1:10000
     ax = -G*M*x0/((x0^2)+(y0^2))^(3/2);                                 
     ay = -G*M*y0/((x0^2)+(y0^2))^(3/2);
-    vx = vx0 + ax0*(T/365);
-    vy = vy0 + ay0*(T/365);
-    x = x0 + vx0*(T/365) + 1/2*ax0*((T/365)^2);
-    y = y0 + vy0*(T/365) + 1/2*ay0*((T/365)^2);
+    vx = vx0 + ax0*(T/10000);
+    vy = vy0 + ay0*(T/10000);
+    x = x0 + vx0*(T/10000) + 1/2*ax0*((T/10000)^2);
+    y = y0 + vy0*(T/10000) + 1/2*ay0*((T/10000)^2);
     ax0 = ax;
     ay0 = ay;
     vx0 = vx;
@@ -62,7 +62,7 @@ end
 hold off
 title('Prob5(c2)');
 xlabel('x');
-ylabel('t(day)');
+ylabel('T/10000');
 
 ax0 = 0;
 ay0 = 0;
@@ -75,10 +75,10 @@ hold on
 for i = 1:10000
     ax = -G*M*x0/((x0^2)+(y0^2))^(3/2);
     ay = -G*M*y0/((x0^2)+(y0^2))^(3/2);
-    vx = vx0 + ax0*(T/365);
-    vy = vy0 + ay0*(T/365);
-    x = x0 + vx0*(T/365) + 1/2*ax0*((T/365)^2);
-    y = y0 + vy0*(T/365) + 1/2*ay0*((T/365)^2);
+    vx = vx0 + ax0*(T/10000);
+    vy = vy0 + ay0*(T/10000);
+    x = x0 + vx0*(T/10000) + 1/2*ax0*((T/10000)^2);
+    y = y0 + vy0*(T/10000) + 1/2*ay0*((T/10000)^2);
     ax0 = ax;
     ay0 = ay;
     vx0 = vx;
@@ -90,27 +90,34 @@ end
 hold off
 title('Prob5(c3)');
 xlabel('y');
-ylabel('t(day)');
+ylabel('T/10000');
 
 %(d)
+
+M = 2*10^30;                        %mass of sun
+m = 6*10^24;                        %mass fo earth
+v = 0.7*2.9822*10^4;                    %speed
+r = G*M/v/v;
+T = 2*pi*r/v;                       %period
+G = 6.67*10^-11;                    %Gravitational constant
+
 
 
 ax0 = 0;
 ay0 = 0;
 vx0 = 0;
 vy0 = 0.7*2.9822*10^4;
-x0 = 1.5*10^11;
+x0 = r;
 y0 = 0;
-T = 2*pi*r/vy0;  
 figure
 hold on
-for i = 1:2000
-    ax = -G*M*x0/((x0^2)+(y0^2))^(3/2);
-    ay = -G*M*y0/((x0^2)+(y0^2))^(3/2);
-    vx = vx0 + ax0*(T/365);
-    vy = vy0 + ay0*(T/365);
-    x = x0 + vx0*(T/365) + 1/2*ax0*((T/365)^2);
-    y = y0 + vy0*(T/365) + 1/2*ay0*((T/365)^2);
+for i = 1:10000
+    ax = -G*M*x0/((x0^2)+(y0^2))^(3/2);                                         %a = F/m
+    ay = -G*M*y0/((x0^2)+(y0^2))^(3/2);                                         %F = GMm/r^2
+    vx = vx0 + ax0*(T/10000);                                                     %v = v0 + at
+    vy = vy0 + ay0*(T/10000);                                                     %x = x0 + vt + 1/2*a*t^2
+    x = x0 + vx0*(T/10000) + 1/2*ax0*((T/10000)^2);
+    y = y0 + vy0*(T/10000) + 1/2*ay0*((T/10000)^2);
     ax0 = ax;
     ay0 = ay;
     vx0 = vx;
@@ -129,17 +136,17 @@ ax0 = 0;
 ay0 = 0;
 vx0 = 0;
 vy0 = 0.7*2.9822*10^4;
-x0 = 1.5*10^11;
+x0 = r;
 y0 = 0;
 figure
 hold on
-for i = 1:2000
-    ax = -G*M*x0/((x0^2)+(y0^2))^(3/2);
+for i = 1:10000
+    ax = -G*M*x0/((x0^2)+(y0^2))^(3/2);                                 
     ay = -G*M*y0/((x0^2)+(y0^2))^(3/2);
-    vx = vx0 + ax0*(T/365);
-    vy = vy0 + ay0*(T/365);
-    x = x0 + vx0*(T/365) + 1/2*ax0*((T/365)^2);
-    y = y0 + vy0*(T/365) + 1/2*ay0*((T/365)^2);
+    vx = vx0 + ax0*(T/10000);
+    vy = vy0 + ay0*(T/10000);
+    x = x0 + vx0*(T/10000) + 1/2*ax0*((T/10000)^2);
+    y = y0 + vy0*(T/10000) + 1/2*ay0*((T/10000)^2);
     ax0 = ax;
     ay0 = ay;
     vx0 = vx;
@@ -151,23 +158,23 @@ end
 hold off
 title('Prob5(d2)');
 xlabel('x');
-ylabel('t(day)');
+ylabel('T/10000');
 
 ax0 = 0;
 ay0 = 0;
 vx0 = 0;
 vy0 = 0.7*2.9822*10^4;
-x0 = 1.5*10^11;
+x0 = r;
 y0 = 0;
 figure
 hold on
-for i = 1:2000
+for i = 1:10000
     ax = -G*M*x0/((x0^2)+(y0^2))^(3/2);
     ay = -G*M*y0/((x0^2)+(y0^2))^(3/2);
-    vx = vx0 + ax0*(T/365);
-    vy = vy0 + ay0*(T/365);
-    x = x0 + vx0*(T/365) + 1/2*ax0*((T/365)^2);
-    y = y0 + vy0*(T/365) + 1/2*ay0*((T/365)^2);
+    vx = vx0 + ax0*(T/10000);
+    vy = vy0 + ay0*(T/10000);
+    x = x0 + vx0*(T/10000) + 1/2*ax0*((T/10000)^2);
+    y = y0 + vy0*(T/10000) + 1/2*ay0*((T/10000)^2);
     ax0 = ax;
     ay0 = ay;
     vx0 = vx;
@@ -179,4 +186,20 @@ end
 hold off
 title('Prob5(d3)');
 xlabel('y');
-ylabel('t');
+ylabel('T/10000');
+%(f)
+
+figure
+hold on
+for i = 0.2:0.0001:1
+
+    v = i*2.9822*10^4;                    %speed
+    r = G*M/v/v;
+    T = 2*pi*r/v;                       %period
+    plot(r*r*r,T*T,'b.');
+
+end
+hold off
+title('Prob5(f)');
+xlabel('T^2');
+ylabel('R^3');
